0979451cd1c75887c8579d3196391b47b243a190
Michael Nett <mnett82@gmail.com>
2016-09-28 22:39:43 +0900

    Globally applied clang-format to all C++ files.

--- indexing/rct/public/dense_vec_data_impl.h
+++ indexing/rct/public/dense_vec_data_impl.h
@@ -2,11 +2,7 @@
 #define INDEXING_RCT_PUBLIC_DENSE_VEC_DATA_H_
 
 namespace indexing {
-namespace rct {
-  
-
-
-}  // namespace rct
+namespace rct {}  // namespace rct
 }  // namespace indexing
-  
+
 #endif  // INDEXING_RCT_PUBLIC_DENSE_VEC_DATA_H_
--- sample/sample.cc
+++ sample/sample.cc
@@ -2,8 +2,8 @@
 
 #include <glog/logging.h>
 
-int main(int argc, char** argv) { 
+int main(int argc, char** argv) {
   google::InitGoogleLogging(argv[0]);
   LOG(INFO) << "Hello, world!";
-  return 0; 
+  return 0;
 }
